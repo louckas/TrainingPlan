9164589bc1263a4a0f254b4718607e7ae53e7d46
Louckas <llouckas@gmail.com>
2023-02-25 12:56:29 +0100

    Added main navigation bar, with support for swipping gesture

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,10 +11,114 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData.from(
+        colorScheme: const ColorScheme.light(),
+      ),
+      darkTheme: ThemeData.from(
+        colorScheme: const ColorScheme.dark(),
+      ),
+      home: const MainBody(),
+    );
+  }
+}
+
+class MainBody extends StatefulWidget {
+  const MainBody({super.key});
+
+  @override
+  State<MainBody> createState() => _MainBodyState();
+}
+
+class _MainBodyState extends State<MainBody> {
+  var menuSelected = 0;
+  @override
+  Widget build(BuildContext context) {
+    Widget menu;
+
+    switch (menuSelected) {
+      case 0:
+        menu = const Placeholder(
+          child: Text("Planning"),
+        );
+        break;
+      case 1:
+        menu = const Placeholder(
+          child: Text("Workout"),
+        );
+        break;
+      case 2:
+        menu = const Placeholder(
+          child: Text("Exercise"),
+        );
+        break;
+      case 3:
+        menu = const Placeholder(
+          child: Text("Overview"),
+        );
+        break;
+      case 4:
+        menu = const Placeholder(
+          child: Text("Settings"),
+        );
+        break;
+      default:
+        throw UnimplementedError('No widget for $menuSelected');
+    }
+
+    return GestureDetector(
+      onHorizontalDragEnd: (details) {
+        setState(() {
+          const speedHisterisis = 20.0;
+          if (details.primaryVelocity! > speedHisterisis) {
+            menuSelected = max((menuSelected - 1), 0);
+          } else if (details.primaryVelocity! < speedHisterisis) {
+            menuSelected = min((menuSelected + 1), 4);
+          }
+        });
+      },
+      child: Scaffold(
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: menu,
+            ),
+            BottomNavigationBar(
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.calendar_month_outlined),
+                  label: "Planning",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.assignment_outlined),
+                  label: "Workout",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.fitness_center),
+                  label: "Exercise",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.auto_graph),
+                  label: "Overview",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.settings),
+                  label: "Settings",
+                ),
+              ],
+              type: BottomNavigationBarType.fixed,
+              currentIndex: menuSelected,
+              selectedItemColor: Colors.amber,
+              onTap: (value) {
+                setState(() {
+                  menuSelected = value;
+                });
+              },
+            ),
+          ],
         ),
       ),
     );
